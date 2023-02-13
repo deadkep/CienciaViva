9a5bc0beb57a3b082d070103ac764a2efbb48ba1
deadkep <125289491+deadkep@users.noreply.github.com>
2023-02-13 15:36:02 +0000

    Add files via upload

---
+++ mapa.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width,initial-scale=1" name="viewport">
+  <meta content="description" name="description">
+  <meta name="google" content="notranslate" />
+  <meta content="Mashup templates have been developped by Orson.io team" name="author">
+
+  <!-- Disable tap highlight on IE -->
+  <meta name="msapplication-tap-highlight" content="no">
+  
+  <link href="./assets/apple-icon-180x180.png" rel="apple-touch-icon">
+  <link href="./assets/favicon.ico" rel="icon">
+
+
+
+  <title>Ciência Viva</title>  
+
+<link href="./main.82cfd66e.css" rel="stylesheet"></head>
+
+<body>
+
+ <!-- Add your content of header -->
+<header class="">
+  <div class="navbar navbar-default visible-xs">
+    <button type="button" class="navbar-toggle collapsed">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a href="./cienciaviva.html" class="navbar-brand">Mashup Template</a>
+  </div>
+
+  <nav class="sidebar">
+    <div class="navbar-collapse" id="navbar-collapse">
+      <div class="site-header hidden-xs">
+          <a class="site-brand" href="./cienciaviva.html" title="">
+            <img class="img-responsive site-logo" alt="" src="./assets/images/mashup-logo.svg">
+            Ciência Viva
+          </a>
+        <p>Ao serviço e sempre alerta para a natureza.</p>
+      </div>
+      <ul class="nav">
+        <li><a href="./cienciaviva.html" title="">Home</a></li>
+        <li><a href="./about.html" title="">Sobre</a></li>
+        <li><a href="./contact.html" title="">Contacto</a></li>
+       <!-- <li><a href="./components.html" title="">Components</a></li> -->
+
+      </ul>
+
+      <nav class="nav-footer">
+        <p class="nav-footer-social-buttons">
+          <a class="fa-icon" href="https://www.instagram.com/" title="">
+            <i class="fa fa-instagram"></i>
+          </a>
+
+        </p>
+        <p><a href="https://www.aeccb.pt/agrupamento/e-s-camilo-castelo-branco/" target="_blank">© AECCB</a>
+      </nav>  
+    </div> 
+  </nav>
+</header>
+<main class="" id="main-collapse">
+
+<?php
+include_once 'header.php';
+include 'locations_model.php';
+//get_unconfirmed_locations();exit;
+?>
+
+    <script type="text/javascript"
+            src="https://maps.googleapis.com/maps/api/js?language=en&key=AIzaSyA-AB-9XZd-iQby-bNLYPFyb0pR2Qw3orw">
+    </script>
+
+    <div id="map"></div>
+    <script>
+        /**
+         * Create new map
+         */
+        var infowindow;
+        var map;
+        var red_icon =  'http://maps.google.com/mapfiles/ms/icons/red-dot.png' ;
+        var orange_icon =  'http://maps.google.com/mapfiles/ms/icons/orange-dot.png' ;
+        var locations = <?php get_confirmed_locations() ?>;
+        var myOptions = {
+            zoom: 17,
+            center: new google.maps.LatLng(41.40778, -8.51157),
+            mapTypeId: 'roadmap'
+        };
+        map = new google.maps.Map(document.getElementById('map'), myOptions);
+
+        /**
+         * Global marker object that holds all markers.
+         * @type {Object.<string, google.maps.LatLng>}
+         */
+        var markers = {};
+
+        /**
+         * Concatenates given lat and lng with an underscore and returns it.
+         * This id will be used as a key of marker to cache the marker in markers object.
+         * @param {!number} lat Latitude.
+         * @param {!number} lng Longitude.
+         * @return {string} Concatenated marker id.
+         */
+        var getMarkerUniqueId= function(lat, lng) {
+            return lat + '_' + lng;
+        };
+
+        /**
+         * Creates an instance of google.maps.LatLng by given lat and lng values and returns it.
+         * This function can be useful for getting new coordinates quickly.
+         * @param {!number} lat Latitude.
+         * @param {!number} lng Longitude.
+         * @return {google.maps.LatLng} An instance of google.maps.LatLng object
+         */
+        var getLatLng = function(lat, lng) {
+            return new google.maps.LatLng(lat, lng);
+        };
+
+        /**
+         * Binds click event to given map and invokes a callback that appends a new marker to clicked location.
+         */
+        var addMarker = google.maps.event.addListener(map, 'click', function(e) {
+            var lat = e.latLng.lat(); // lat of clicked point
+            var lng = e.latLng.lng(); // lng of clicked point
+            var markerId = getMarkerUniqueId(lat, lng); // an that will be used to cache this marker in markers object.
+            var marker = new google.maps.Marker({
+                position: getLatLng(lat, lng),
+                map: map,
+                animation: google.maps.Animation.DROP,
+                id: 'marker_' + markerId,
+                html: "    <div id='info_"+markerId+"'>\n" +
+                "        <table class=\"map1\">\n" +
+                "            <tr>\n" +
+                "                <td><a>Description:</a></td>\n" +
+                "                <td><textarea  id='manual_description' placeholder='Description'></textarea></td></tr>\n" +
+                "            <tr><td></td><td><input type='button' value='Save' onclick='saveData("+lat+","+lng+")'/></td></tr>\n" +
+                "        </table>\n" +
+                "    </div>"
+            });
+            markers[markerId] = marker; // cache marker in markers object
+            bindMarkerEvents(marker); // bind right click event to marker
+            bindMarkerinfo(marker); // bind infowindow with click event to marker
+        });
+
+        /**
+         * Binds  click event to given marker and invokes a callback function that will remove the marker from map.
+         * @param {!google.maps.Marker} marker A google.maps.Marker instance that the handler will binded.
+         */
+        var bindMarkerinfo = function(marker) {
+            google.maps.event.addListener(marker, "click", function (point) {
+                var markerId = getMarkerUniqueId(point.latLng.lat(), point.latLng.lng()); // get marker id by using clicked point's coordinate
+                var marker = markers[markerId]; // find marker
+                infowindow = new google.maps.InfoWindow();
+                infowindow.setContent(marker.html);
+                infowindow.open(map, marker);
+                // removeMarker(marker, markerId); // remove it
+            });
+        };
+
+        /**
+         * Binds right click event to given marker and invokes a callback function that will remove the marker from map.
+         * @param {!google.maps.Marker} marker A google.maps.Marker instance that the handler will binded.
+         */
+        var bindMarkerEvents = function(marker) {
+            google.maps.event.addListener(marker, "rightclick", function (point) {
+                var markerId = getMarkerUniqueId(point.latLng.lat(), point.latLng.lng()); // get marker id by using clicked point's coordinate
+                var marker = markers[markerId]; // find marker
+                removeMarker(marker, markerId); // remove it
+            });
+        };
+
+        /**
+         * Removes given marker from map.
+         * @param {!google.maps.Marker} marker A google.maps.Marker instance that will be removed.
+         * @param {!string} markerId Id of marker.
+         */
+        var removeMarker = function(marker, markerId) {
+            marker.setMap(null); // set markers setMap to null to remove it from map
+            delete markers[markerId]; // delete marker instance from markers object
+        };
+
+
+        /**
+         * loop through (Mysql) dynamic locations to add markers to map.
+         */
+        var i ; var confirmed = 0;
+        for (i = 0; i < locations.length; i++) {
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                map: map,
+                icon :   locations[i][4] === '1' ?  red_icon  : orange_icon,
+                html: "<div>\n" +
+                "<table class=\"map1\">\n" +
+                "<tr>\n" +
+                "<td><a>Description:</a></td>\n" +
+                "<td><textarea disabled id='manual_description' placeholder='Description'>"+locations[i][3]+"</textarea></td></tr>\n" +
+                "</table>\n" +
+                "</div>"
+            });
+
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    infowindow = new google.maps.InfoWindow();
+                    confirmed =  locations[i][4] === '1' ?  'checked'  :  0;
+                    $("#confirmed").prop(confirmed,locations[i][4]);
+                    $("#id").val(locations[i][0]);
+                    $("#description").val(locations[i][3]);
+                    $("#form").show();
+                    infowindow.setContent(marker.html);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+        }
+
+        /**
+         * SAVE save marker from map.
+         * @param lat  A latitude of marker.
+         * @param lng A longitude of marker.
+         */
+        function saveData(lat,lng) {
+            var description = document.getElementById('manual_description').value;
+            var url = 'locations_model.php?add_location&description=' + description + '&lat=' + lat + '&lng=' + lng;
+            downloadUrl(url, function(data, responseCode) {
+                if (responseCode === 200  && data.length > 1) {
+                    var markerId = getMarkerUniqueId(lat,lng); // get marker id by using clicked point's coordinate
+                    var manual_marker = markers[markerId]; // find marker
+                    manual_marker.setIcon(orange_icon);
+                    infowindow.close();
+                    infowindow.setContent("<div style=' color: orange; font-size: 25px;'> Waiting for admin confirm!!</div>");
+                    infowindow.open(map, manual_marker);
+
+                }else{
+                    console.log(responseCode);
+                    console.log(data);
+                    infowindow.setContent("<div style='color: red; font-size: 25px;'>Inserting Errors</div>");
+                }
+            });
+        }
+
+        function downloadUrl(url, callback) {
+            var request = window.ActiveXObject ?
+                new ActiveXObject('Microsoft.XMLHTTP') :
+                new XMLHttpRequest;
+
+            request.onreadystatechange = function() {
+                if (request.readyState == 4) {
+                    callback(request.responseText, request.status);
+                }
+            };
+
+            request.open('GET', url, true);
+            request.send(null);
+        }
+
+
+    </script>
+
+
+
+
+
+<?php
+include_once 'footer.php';
+
+?>
+<!--
+  <div class="col-xs-12 col-md-4 section-container-spacer">
+    <img class="img-responsive" alt="" src="./assets/images/img-12.jpg">
+    <h2>Consectetur</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <a href="./contact.html" class="btn btn-primary" title=""> Get in touch</a>
+  </div>
+
+  <div class="col-xs-12 col-md-4 section-container-spacer">
+    <img class="img-responsive" alt="" src="./assets/images/img-13.jpg">
+    <h2>Adipiscing</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <a href="./contact.html" class="btn btn-primary" title=""> Get in touch</a>
+  </div>
+
+  <div class="col-xs-12 col-md-4 section-container-spacer">
+    <img class="img-responsive" alt="" src="./assets/images/img-14.jpg">
+    <h2>Dolor</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <a href="./contact.html" class="btn btn-primary" title=""> Get in touch</a>
+  </div>
+
+</div>
+-->
+
+</main>
+
+<script>
+document.addEventListener("DOMContentLoaded", function (event) {
+  navbarToggleSidebar();
+  navActivePage();
+});
+</script>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID 
+
+<script>
+  (function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+  ga('create', 'UA-XXXXX-X', 'auto');
+  ga('send', 'pageview');
+</script>
+
+--> <script type="text/javascript" src="./main.85741bff.js"></script></body>
+
+</html>
